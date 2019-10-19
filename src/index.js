178d82e6921594a826c9000901c18a43b80f76c2
themithy <11431578+themithy@users.noreply.github.com>
2019-10-19 20:35:48 +0200

    Add sample code.

---
+++ src/index.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return null
+}
+
+const root = document.createElement('div')
+document.body.appendChild(root)
+
+ReactDOM.render(<App />, root) 
+
